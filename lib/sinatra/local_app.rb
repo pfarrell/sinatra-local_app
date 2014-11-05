77d237118d313dac355aaa2f31acd4df50fc9718
Patrick Farrell <pfarrell@prosper.com>
2014-11-04 16:07:01 -0800

    local redirect support

--- lib/sinatra/local_app.rb
+++ lib/sinatra/local_app.rb
@@ -6,6 +6,11 @@ module Sinatra
     def self.registered(app) 
       app.set :local_ip, '127.0.0.1'
       app.set :local_port, '9292'
+
+      app.get '/local' do
+        redirect to(request.url.gsub(request.host_with_port, "#{settings.local_ip}:#{settings.local_port}").gsub('/local',''))
+      end
     end
+
   end
 end
--- spec/extension_spec.rb
+++ spec/extension_spec.rb
@@ -8,4 +8,13 @@ describe Sinatra::LocalApp do
   it "should initialize with :local_port set to 9292" do
     expect(TestApp.local_port).to eq('9292')
   end
+
+  it "should respond to '/local'" do
+    get '/local'
+  end
+
+  it "should redirect requests to '/local'" do
+    get '/local'
+    expect(last_response).to be_redirect
+  end
 end
