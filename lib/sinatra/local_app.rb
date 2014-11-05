d55ce537593c1115bbe10c580ae2da585b504b96
Patrick Farrell <pfarrell@prosper.com>
2014-11-04 17:01:45 -0800

    changed to local_authority (http://en.wikipedia.org/wiki/URI_scheme)

--- lib/sinatra/local_app.rb
+++ lib/sinatra/local_app.rb
@@ -4,11 +4,11 @@ module Sinatra
   module LocalApp
 
     def self.registered(app) 
-      app.set :local_ip, '127.0.0.1'
-      app.set :local_port, '9292'
+      app.set :local_authority, '127.0.0.1:9292' # http://en.wikipedia.org/wiki/URI_scheme 
 
       app.get '/local' do
-        redirect to(request.url.gsub(request.host_with_port, "#{settings.local_ip}:#{settings.local_port}").gsub('/local',''))
+        new_url = request.url.gsub("#{request.host_with_port}/local", "#{settings.local_authority}")
+        redirect to(new_url)
       end
     end
 
--- spec/extension_spec.rb
+++ spec/extension_spec.rb
@@ -1,12 +1,8 @@
 require 'spec_helper'
 
 describe Sinatra::LocalApp do
-  it "should initialize with :local_ip set to 127.0.0.1" do
-    expect(TestApp.local_ip).to eq('127.0.0.1')
-  end
-
-  it "should initialize with :local_port set to 9292" do
-    expect(TestApp.local_port).to eq('9292')
+  it "should initialize with :local_authority set to 127.0.0.1" do
+    expect(TestApp.local_authority).to eq('127.0.0.1:9292')
   end
 
   it "should respond to '/local'" do
