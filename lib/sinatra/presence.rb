0df753c30d72d85576610d9c0afc31df72e2e9d1
Patrick Farrell <pfarrell@prosper.com>
2014-11-05 08:32:35 -0800

    Added cookie to requests for local redirect support

--- lib/sinatra/presence.rb
+++ lib/sinatra/presence.rb
@@ -6,9 +6,15 @@ module Sinatra
     def self.registered(app) 
       app.set :local_authority, '127.0.0.1:9292' # http://en.wikipedia.org/wiki/URI_scheme 
 
+      app.before do
+        if(request.cookies["local_url"].nil?)
+          new_url = request.url.gsub("#{request.host_with_port}", "#{settings.local_authority}").gsub("/local", "") 
+          response.set_cookie(:local_url, value: new_url, expires: Time.now + 600)
+        end
+      end
+
       app.get '/local' do
-        new_url = request.url.gsub("#{request.host_with_port}", "#{settings.local_authority}")
-          .gsub("/local", "") 
+        new_url = request.url.gsub("#{request.host_with_port}", "#{settings.local_authority}").gsub("/local", "") 
         redirect to(new_url)
       end 
     end 
