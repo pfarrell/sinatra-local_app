e7f640cf78e560e0501dd2dae41680036c9c8290
Patrick Farrell <pfarrell@prosper.com>
2014-11-04 19:45:41 -0800

    fixed issue with interaction with url_for... domain/app/local should resolve to localdomain/app

--- lib/sinatra/local_app.rb
+++ lib/sinatra/local_app.rb
@@ -7,10 +7,11 @@ module Sinatra
       app.set :local_authority, '127.0.0.1:9292' # http://en.wikipedia.org/wiki/URI_scheme 
 
       app.get '/local' do
-        new_url = request.url.gsub("#{request.host_with_port}/local", "#{settings.local_authority}")
+        new_url = request.url.gsub("#{request.host_with_port}", "#{settings.local_authority}")
+          .gsub("/local", "") 
         redirect to(new_url)
-      end
-    end
+      end 
+    end 
 
-  end
+  end 
 end
